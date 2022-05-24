3262b2bc844bec328ac0469d80a7ac82e6dd3a5d
Cesar Nogueira <cesardinogueira@gmail.com>
2022-05-24 15:51:52 -0300

    Requisito 01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import logo from './trivia.png';
+import Login from './pages/Login';
 import './App.css';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <Switch>
+            <Route exact path="/" component={ Login } />
+          </Switch>
+        </header>
+      </div>
+    );
+  }
 }
+
+export default App;
