cb97a121528a122b369a0cc4d0a0a92f66efc92d
Stephani Rocha <gomes.stephani19@gmail.com>
2022-05-24 22:03:08 -0300

    Corrige problemas com o requisito 2

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -9,19 +9,19 @@ class Game extends Component {
   } */
 
   render() {
-    const data = JSON.parse(localStorage.getItem('info'));
+    const data = JSON.parse(localStorage.getItem('ranking'));
     console.log('data', data);
     return (
       <div>
         <header />
         <h1>GAME</h1>
         <img
-          src={ data.ranking[0].picture }
+          src={ data[0].picture }
           alt="Imagem-Token"
           data-testid="header-profile-picture"
         />
-        <p data-testid="header-player-name">{ data.ranking[0].name }</p>
-        <p data-testid="header-score">{ data.ranking[0].score }</p>
+        <p data-testid="header-player-name">{ data[0].name }</p>
+        <p data-testid="header-score">{ data[0].score }</p>
       </div>
     );
   }
