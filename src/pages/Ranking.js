f174b15080fa2fe2e37169ace0f970794dadd5e7
Franciane Menestrina <francianemenestrina@gmail.com>
2022-05-27 11:42:31 -0300

    Requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
+  onClickInicio = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          onClick={ this.onClickInicio }
+          data-testid="btn-go-home"
+        >
+          Início
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Ranking;
