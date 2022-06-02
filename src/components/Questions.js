d3bca38324705c478de847f6e52e2693e0c77e01
Adriano Costa <adrianofacosta@gmail.com>
2022-06-02 11:26:53 -0300

    lint: alterado comprimento do código na linha 39 e 55 de Questions.js

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -12,57 +12,71 @@ class Questions extends Component {
         {!questions ? null : (
           <div>
             <div className="div-time">
-              <p>{ over ? <p className="p-gameOver">GAME OVER</p> : `0:${countDown}` }</p>
+              <p>
+                {over ? (
+                  <p className="p-gameOver">GAME OVER</p>
+                ) : (
+                  `0:${countDown}`
+                )}
+              </p>
             </div>
             <div className="card-game">
               <div className="div-category div-font">
                 <p data-testid="question-category">{questions.category}</p>
               </div>
               <div className="div-questions div-font">
-                <p data-testid="question-text">{he.decode(questions.question)}</p>
+                <p data-testid="question-text">
+                  {he.decode(questions.question)}
+                </p>
               </div>
-              <div data-testid="answer-options" className="div-answers div-font">
-                {
-                  respostas.map(
-                    (resposta, index) => (questions.correct_answer === resposta
-                      ? (
-                        <button
-                          type="button"
-                          className="btn-answer"
-                          key={ index }
-                          disabled={ disable }
-                          data-testid="correct-answer"
-                          value={ resposta }
-                          onClick={ handleOnClick }
-                          style={
-                            css
-                              ? { border: '3px solid rgb(6, 240, 15)', color: 'black', backgroundColor: 'rgb(6, 240, 15)' }
-                              : { color: 'black' }
+              <div
+                data-testid="answer-options"
+                className="div-answers div-font"
+              >
+                {respostas.map(
+                  (resposta, index) => (questions.correct_answer === resposta ? (
+                    <button
+                      type="button"
+                      className="btn-answer"
+                      key={ index }
+                      disabled={ disable }
+                      data-testid="correct-answer"
+                      value={ resposta }
+                      onClick={ handleOnClick }
+                      style={
+                        css
+                          ? {
+                            border: '3px solid rgb(6, 240, 15)',
+                            color: 'black',
+                            backgroundColor: 'rgb(6, 240, 15)',
                           }
-                        >
-                          {he.decode(resposta)}
-                        </button>
-                      ) : (
-                        <button
-                          type="button"
-                          className="btn-answer"
-                          key={ index }
-                          disabled={ disable }
-                          data-testid={ `wrong-answer-${index}` }
-                          value={ resposta }
-                          onClick={ handleOnClick }
-                          style={
-                            css ? { border: '3px solid red', color: 'black' } : { color: 'black' }
-                          }
-                        >
-                          {he.decode(resposta)}
-                        </button>
-                      )),
-                  )
-                }
+                          : { color: 'black' }
+                      }
+                    >
+                      {he.decode(resposta)}
+                    </button>
+                  ) : (
+                    <button
+                      type="button"
+                      className="btn-answer"
+                      key={ index }
+                      disabled={ disable }
+                      data-testid={ `wrong-answer-${index}` }
+                      value={ resposta }
+                      onClick={ handleOnClick }
+                      style={
+                        css
+                          ? { border: '3px solid red', color: 'black' }
+                          : { color: 'black' }
+                      }
+                    >
+                      {he.decode(resposta)}
+                    </button>
+                  )),
+                )}
               </div>
               <div className="div-next div-font">
-                { btnNext && (
+                {btnNext && (
                   <button
                     data-testid="btn-next"
                     type="submit"
@@ -70,7 +84,8 @@ class Questions extends Component {
                     onClick={ onClickNext }
                   >
                     Next
-                  </button>)}
+                  </button>
+                )}
               </div>
             </div>
           </div>
